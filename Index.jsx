1d9c2d7569cf8a3df659e642bebbd13db9eafcec
Anuchiya <155542485+Anuchiya@users.noreply.github.com>
2024-10-12 09:17:27 +0530

    Update Index.jsx

--- Index.jsx
+++ Index.jsx
@@ -1,1 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+const routes = require('./routes/index');
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api', routes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err =>
